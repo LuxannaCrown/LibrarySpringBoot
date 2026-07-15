34af14f3e58d8b2943c0ca9141b458291570c6fa
LuxannaCrown <leonbehrmann@yahoo.de>
2026-07-15 14:59:38 +0200

    initial commit

---
+++ src/main/java/com/starfinanz/LibraryBeanWithSpringBoot/LibraryBeanWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.starfinanz.LibraryBeanWithSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryBeanWithSpringBootApplication {
+
+	void main() {
+		SpringApplication.run(LibraryBeanWithSpringBootApplication.class);
+	}
+
+}
---
+++ src/test/java/com/starfinanz/LibraryBeanWithSpringBoot/LibraryBeanWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starfinanz.LibraryBeanWithSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryBeanWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
